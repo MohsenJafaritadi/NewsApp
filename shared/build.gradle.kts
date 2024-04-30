9c47175b4b46687ea344c158012c22b2c175e7ca
dev_mohsenJafari <dev.mohsenjafaritadi@gmail.com>
2024-04-30 15:45:15 +0330

    edit project build.gradle.kts

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 kotlin {
     androidTarget()
 
-    jvm("desktop")
+
 
     listOf(
         iosX64(),
@@ -32,7 +32,8 @@ kotlin {
                 @OptIn(org.jetbrains.compose.ExperimentalComposeLibrary::class)
                 implementation(compose.components.resources)
 
-
+                api("io.insert-koin:koin-core:3.4.3")
+                api("io.insert-koin:koin-compose:1.0.4")
                 val ktor = "2.2.2"
 
                 implementation("io.ktor:ktor-client-core:${ktor}")
@@ -42,6 +43,39 @@ kotlin {
                 implementation("io.ktor:ktor-client-content-negotiation:${ktor}")
                 implementation("io.ktor:ktor-serialization-kotlinx-json:${ktor}")
                 implementation("io.ktor:ktor-client-resources:${ktor}")
+
+
+                api("dev.icerock.moko:mvvm-core:0.16.1") // only ViewModel, EventsDispatcher, Dispatchers.UI
+                api("dev.icerock.moko:mvvm-compose:0.16.1") // api mvvm-core, getViewModel for Compose Multiplatfrom
+
+                // endregion
+
+                // region arrow
+
+                val arrow = "1.0.1"
+
+                implementation("io.arrow-kt:arrow-core:${arrow}")
+                implementation("io.arrow-kt:arrow-fx-coroutines:${arrow}")
+                implementation("io.arrow-kt:arrow-fx-stm:${arrow}")
+
+
+                // region Kotlinx
+
+                implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.3")
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
+                implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.4.0")
+                implementation("org.jetbrains.kotlinx:kotlinx-collections-immutable:0.3.5")
+
+                // endregion
+
+                // region Decompose
+
+                implementation("io.github.xxfast:decompose-router:0.4.0")
+                implementation("com.arkivanov.decompose:decompose:2.1.2")
+                implementation("com.arkivanov.decompose:extensions-compose-jetbrains:2.1.0-compose-experimental-alpha-07")
+                implementation("com.arkivanov.essenty:parcelable:1.2.0")
+
+                implementation("media.kamel:kamel-image:0.9.1")
             }
         }
         val androidMain by getting {
@@ -50,8 +84,12 @@ kotlin {
                 api("androidx.appcompat:appcompat:1.6.1")
                 api("androidx.core:core-ktx:1.10.1")
 
+
+                api("io.insert-koin:koin-android:3.4.3")
+
                 implementation("io.ktor:ktor-client-android:2.2.2")
                 implementation("io.ktor:ktor-client-okhttp:2.2.2")
+
             }
         }
         val iosX64Main by getting
@@ -63,7 +101,15 @@ kotlin {
             iosArm64Main.dependsOn(this)
             iosSimulatorArm64Main.dependsOn(this)
             dependencies{
+
                 implementation("io.ktor:ktor-client-darwin:2.2.2")
+//
+//                implementation("com.squareup.sqldelight:native-driver:1.5.3")
+//
+//                implementation("io.github.xxfast:decompose-router:0.4.0")
+//                implementation("com.arkivanov.decompose:decompose:2.1.2")
+//                implementation("com.arkivanov.decompose:extensions-compose-jetbrains:2.1.0-compose-experimental-alpha-07")
+//                implementation("com.arkivanov.essenty:parcelable:1.2.0")
             }
         }
     }
